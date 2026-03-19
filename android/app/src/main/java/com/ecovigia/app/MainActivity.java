9204455300002e8c8af97bfc00341a549b86d3eb
Eduardo Cruz <ce.cruz1@hotmail.com>
2026-03-18 19:07:56 -0500

    encapsulamiento con capacitor y vite para generar apk, ajuste de responsividad en moviles

---
+++ android/app/src/main/java/com/ecovigia/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ecovigia.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config = {
+  appId: 'com.ecovigia.app',
+  appName: 'ecovigia-humedal-app',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
